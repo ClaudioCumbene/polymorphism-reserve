93d3768671cb035620efb243bf2a6e9e9aed4fb0
ClaudioCumbene <ClaudioCumbene@gmail>
2024-08-27 17:40:05 +0200

    Segunda versao do projecto

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -8,11 +8,11 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
-	private static SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy"); 
-	
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 	public Reservation() {
-		
+
 	}
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
@@ -36,30 +36,32 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duraction() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public void updateDates(Date checkIn, Date checkOut) {
-		this.checkIn= checkIn;
-		this.checkOut= checkOut;
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		} 
+		if (!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
+		
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room: "
-				+roomNumber
-				+", Check-in: "
-				+sdf.format(checkIn)
-				+", Check-out: "
-				+sdf.format(checkOut)
-				+", "
-				+duraction()														
-				+" nights";
-				
-				
-	} 
+		return "Room: " + roomNumber + ", Check-in: " + sdf.format(checkIn) + ", Check-out: " + sdf.format(checkOut)
+				+ ", " + duraction() + " nights";
+
+	}
 
 }
